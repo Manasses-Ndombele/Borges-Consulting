a65449fcb576f9bca722805584085e21522701cf
Manassés Ndombele <manassesndombelefreelancer@gmail.com>
2025-01-31 23:44:36 +0000

    Otimizei todas imagens e adicionei favicon no site

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -11,8 +11,8 @@ export default function Clients() {
                     <img src="/boomsales.png" alt="Cliente 2" />
                     <img src="/criptomaster.png" alt="Cliente 3" />
                     <img src="/didacta.png" alt="Cliente 4" />
-                    <img src="/megamed.png" alt="Cliente 5" />
-                    <img src="/raceshadows.png" alt="Cliente 6" />
+                    <img src="/mega_med.png" alt="Cliente 5" />
+                    <img src="/race_shadows.png" alt="Cliente 6" />
                 </div>
             </div>
         </div>
--- src/components/Company.jsx
+++ src/components/Company.jsx
@@ -3,7 +3,7 @@ import '../assets/components/company.sass';
 export default function Company() {
     return (
         <div id="company-area" className="p-2 area d-md-flex mb-md-5 mx-lg-auto align-items-lg-center justify-content-lg-start mx-xl-auto">
-            <img src="/business.jpeg" alt="Exemplo de funcionários da empresa trabalhando" className="w-100 my-2" />
+            <img src="/business.jpg" alt="Exemplo de funcionários da empresa trabalhando" className="w-100 my-2" />
             <div className="more-info p-3 m-2 m-lg-0 d-lg-flex justify-content-lg-center flex-lg-column">
                 <h2>Uma empresa especializada em renovar o seu negócio</h2>
                 <p className="p-lg-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores deleniti, eligendi aperiam ipsam repudiandae tenetur blanditiis numquam iure at quo animi amet soluta laboriosam sapiente a. Quibusdam assumenda quae sed?</p>
